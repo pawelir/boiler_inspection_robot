c37fbeba2e72050a843f8baecaf3686a806e68c2
pawelir <pawel.irzyk6@gmail.com>
2023-11-19 14:59:24 +0100

    Rename package

---
+++ boiler_inspection_robot_controller/src/boiler_inspection_robot_controller_node.py
@@ -0,0 +1,78 @@
+#! /usr/bin/env python3
+
+from geometry_msgs.msg import Twist
+from math import pi
+from std_msgs.msg import Float64, Int32MultiArray
+import rospy
+
+
+class RobotManagerNode:
+    def __init__(self):
+        self._load_params()
+        self.pub_cmd_vel_fl = rospy.Publisher(
+            "/boiler_inspection_robot/left_front_wheel_velocity_controller/command",
+            Float64,
+            queue_size=1,
+        )
+        self.pub_cmd_vel_rl = rospy.Publisher(
+            "/boiler_inspection_robot/left_rear_wheel_velocity_controller/command",
+            Float64,
+            queue_size=1,
+        )
+        self.pub_cmd_vel_fr = rospy.Publisher(
+            "/boiler_inspection_robot/right_front_wheel_velocity_controller/command",
+            Float64,
+            queue_size=1,
+        )
+        self.pub_cmd_vel_rr = rospy.Publisher(
+            "/boiler_inspection_robot/right_rear_wheel_velocity_controller/command",
+            Float64,
+            queue_size=1,
+        )
+        self.pub_cmd_vel_epos = rospy.Publisher(
+            "/boiler_inspection_robot/epos_cmd_vel", Int32MultiArray, queue_size=1
+        )
+        self.cmd_vel_sub = rospy.Subscriber(
+            "/cmd_vel", Twist, self._update_robot_velocity
+        )
+        self.epos_velocity_msg = Int32MultiArray()
+
+    def _load_params(self) -> None:
+        self.track_of_wheels = rospy.get_param(
+            "boiler_inspection_robot_controller/track_of_wheels"
+        )
+        self.wheel_radius = rospy.get_param(
+            "boiler_inspection_robot_controller/wheel_radius"
+        )
+
+    def _update_robot_velocity(self, msg: Twist) -> None:
+        self.calculate_wheels_veocity(msg.linear.x, msg.angular.z)
+        self.publish_controller_commands()
+
+    def calculate_wheels_veocity(self, V: float, w: float) -> None:
+        self.wheel_speed_left = (2 * V - w * self.track_of_wheels) / (
+            2 * self.wheel_radius
+        )
+        self.wheel_speed_right = (2 * V + w * self.track_of_wheels) / (
+            2 * self.wheel_radius
+        )
+        self.epos_velocity_msg.data = [
+            int(self.wheel_speed_left / (2 * pi) * 60),
+            int(self.wheel_speed_right / (2 * pi) * 60),
+        ]  # EPOS controller accept rpm
+
+    def publish_controller_commands(self) -> None:
+        self.pub_cmd_vel_fl.publish(self.wheel_speed_left)
+        self.pub_cmd_vel_rl.publish(self.wheel_speed_left)
+        self.pub_cmd_vel_fr.publish(self.wheel_speed_right)
+        self.pub_cmd_vel_rr.publish(self.wheel_speed_right)
+        self.pub_cmd_vel_epos.publish(self.epos_velocity_msg)
+
+
+if __name__ == "__main__":
+    rospy.init_node("boiler_inspection_robot_controller")
+    try:
+        boiler_inspection_robot_controller_node = RobotManagerNode()
+    except rospy.ROSInterruptException:
+        pass
+    rospy.spin()
